6b558ac58ac313c005a46dadd4b9b4e9f6b3c310
lonelysamu <quahjit@gmail.com>
2023-09-06 14:55:18 +0800

    Add files via upload

--- qrcodescanner.js
+++ qrcodescanner.js
@@ -1,8 +1,9 @@
 function onScanSuccess(decodedText, decodedResult) {
-    console.log(`Code scanned = ${decodedText}`, decodedResult);
     window.open(`${decodedText}`);
     html5QrcodeScanner.clear();
+
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
-	"qr-reader", { fps: 10, qrbox: 250 });
+    "qr-reader", { fps: 10, qrbox: 250 });
+
 html5QrcodeScanner.render(onScanSuccess);
